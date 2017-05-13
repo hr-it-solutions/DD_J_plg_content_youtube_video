e8138540e0f463a2350c1891367d7a654935f159
Florian Häusler <florian@didldu.com>
2017-05-13 14:27:52 +0200

    Added scriptfile

---
+++ script.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @package    DD_YouTube_Video
+ *
+ * @author     HR IT-Solutions Florian Häusler <info@hr-it-solutions.com>
+ * @copyright  Copyright (C) 2017 - 2017 Didldu e.K. | HR IT-Solutions
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die();
+
+/**
+ * Class PlgContentDD_YouTube_VideoInstallerScript
+ *
+ * @since  Version  1.0.0.0
+ */
+
+class PlgContentDD_YouTube_VideoInstallerScript
+{
+	protected $name = 'DD YouTube Vide';
+
+	protected $extensionsToEnable = array(
+
+		array(  'name'  => 'dd_youtube_video',
+				'type'  => 'plugin',
+				'group' => 'system')
+
+	);
+
+	/**
+	 * Enable extensions
+	 *
+	 * @since Version  1.0.0.0
+	 *
+	 * @return void
+	 */
+	private function enableExtensions()
+	{
+		foreach ($this->extensionsToEnable as $extension)
+		{
+			$db  = JFactory::getDbo();
+			$query = $db->getQuery(true)
+					->update('#__extensions')
+					->set($db->qn('enabled') . ' = ' . $db->q(1))
+					->where('type = ' . $db->q($extension['type']))
+					->where('element = ' . $db->q($extension['name']));
+
+			if ($extension['type'] === 'plugin')
+			{
+				$query->where('folder = ' . $db->q($extension['group']));
+			}
+
+			$db->setQuery($query);
+			$db->execute();
+		}
+	}
+
+	/**
+	 * JInstaller
+	 *
+	 * @param   object  $parent  \JInstallerAdapterPackageParent
+	 *
+	 * @return  boolean
+	 *
+	 * @since Version  1.0.0.0
+	 */
+	public function install($parent)
+	{
+		$this->enableExtensions();
+
+		return true;
+	}
+}
